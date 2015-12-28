ad7568c5277559e28d282d945d9684462a8d30f9
dengchenglin <297854263@qq.com>
2015-12-28 10:47:52 +0800

    Initial Commit

---
+++ MultilevelTableVIew/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MultilevelTableVIew
+//
+//  Created by chenguangjiang on 15/12/28.
+//  Copyright © 2015年 dengchenglin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
